a2fb078be8a6e73798f70e6da1e3df5ac70fb618
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-05-18 18:32:40 +0200

    debut navbar

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 const routes: Routes = [];
 
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './shared/header/header.component';
 import { NavbarComponent } from './shared/header/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from './shared/main-content/main-content.component';
+
 import { HomeComponent } from './home/home.component';
+
 import { AboutComponent } from './about/about.component';
+
 import { SkillsComponent } from './skills/skills.component';
 import { SkillsBoxComponent } from './skills/skills-box/skills-box.component';
 import { SkillsItemComponent } from './skills/skills-item/skills-item.component';
+
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsItemComponent } from './projects/projects-item/projects-item.component';
+
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
