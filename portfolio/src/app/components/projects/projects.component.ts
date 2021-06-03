e59cdc606dda31d6591cfaeeb120b8add8db27a6
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-06-03 21:15:29 +0200

    clean "projects" + ajout des clics sur les dots

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {ProjectsService} from "../../services/projects.service";
-import {Project} from "../../models/project.models";
+import { Component } from '@angular/core';
+import { ProjectsService } from "../../services/projects.service";
+import { Project } from "../../models/project.models";
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent  {
 
+  // récupération des projets via le service
   slides: Project[] = this.projectsService.getProjects();
+  // index du projet actuellement affiché
   currentSlide = 0;
+  // permet de gérer les index
   numbers: number[];
 
   constructor(private projectsService: ProjectsService) {
     this.numbers = Array(this.slides.length).fill(0).map((x, i) =>i);
   }
 
-  // récupération des projets via le service
-  ngOnInit(): void { }
-
+  // méthode qui permet de passer au slide précédent
   onPrevClick() {
     const previous = this.currentSlide - 1;
     this.currentSlide = previous < 0 ? this.slides.length - 1: previous;
@@ -27,9 +28,13 @@ export class ProjectsComponent implements OnInit {
 
   }
 
+  // méthode qui permet de passer au slide suivant
   onNextClick() {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
     console.log('Slide suivante', this.currentSlide);
   }
+
+  // // méthode qui permet de passer au slide correspond au numéro du dot cliqué
+  onIndexClick(i: number) { this.currentSlide = i; }
 }
